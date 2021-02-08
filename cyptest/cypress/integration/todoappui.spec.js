b45d92211cf388e6caad10fa45ecd58a791b94e0
m3l1k4 <melikaihelas@gmail.com>
2021-02-08 14:05:04 -0800

    visit url

--- cyptest/cypress/integration/todoappui.spec.js
+++ cyptest/cypress/integration/todoappui.spec.js
@@ -1,5 +1,9 @@
 describe('name of the group', () => {
-   it('should', ()=> {
-expect(true).equal(true)
+    it('should', () => {
+        expect(true).equal(true)
+    })
+
+    it('should', ()=>{
+        cy.visit('http://localhost:3000/')
     })
 })
